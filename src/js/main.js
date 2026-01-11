5c1967ecefc12e1c80d46745b08b92cafc1875ed
outtaspacecode <cole.beebe@protonmail.com>
2026-01-10 20:34:22 -0500

    Completed activities for NPS - part 1

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,21 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+
+const disclaimerLink = document.querySelector(".disclaimer > a");
+disclaimerLink.textContent = parkData.fullName;
+disclaimerLink.href = parkData.url;
+
+document.title = parkData.fullName;
+
+const heroImage = document.querySelector(".hero > img");
+const imageInfo = parkData.images[0];
+heroImage.src = imageInfo.url;
+heroImage.alt = imageInfo.altText;
+
+const heroTitle = document.querySelector(".hero-title");
+heroTitle.textContent = parkData.name;
+const designation = document.querySelector(".park-designation");
+designation.textContent = parkData.designation;
+const states = document.querySelector(".park-states");
+states.textContent = parkData.states;
