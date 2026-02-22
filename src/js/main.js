18d4878965147e572c05344ec2e610bd29e0a02f
outtaspacecode <cole.beebe@protonmail.com>
2026-02-21 23:29:20 -0500

    Finished w07

--- src/js/main.js
+++ src/js/main.js
@@ -5,21 +5,28 @@ import '../css/home.css';
 
 
 function enableNavigation() {
-    // Use a querySelector to get the menu buttons
+  const menuButton = document.querySelector("#global-nav-toggle");
+  
+  // when the main menu button is clicked:
+  menuButton.addEventListener("click", (ev) => {
+    let target = ev.target;
+  // toggle the show class on the global-nav
+  document.querySelector(".global-nav").classList.toggle("show");
+  // check to see if ev.target is the button or something inside the button
+  if (target.tagName != "BUTTON") {
+    target = target.closest("button");
+  }
+  // check to see if we just opened or closed the menu
+  if (document.querySelector(".global-nav").classList.contains("show")) {
+    // if we opened it then set the aria-expanded attribute to true
+    target.setAttribute("aria-expanded", true);
+  } else {
+    // if we closed it then set the aria-expanded attribute to false
+    target.setAttribute("aria-expanded", false);
+  }
 
-    // When the main menu button is clicked:
-    menuButton.addEventListener("click", (e) => {
-        let target = e.target;
-        // Toggle the show class on the global-nav
-
-        // Check to see if target is the button or something inside the button
-
-        // Check to see if we just opened or closed the menu
-
-        // If we opened it then set the aria-expanded attribute on the button to true
-
-        // If we closed it then set the aria-expanded attribute on the button to false
-    })
+  console.log("toggle");
+  });
 }
 
 
@@ -45,6 +52,8 @@ async function init() {
     setHeaderFooter(parkData);
     setParkInfo(parkData);
     setParkInfoLinks(links);
+
+    enableNavigation();
 }
 
 init();
