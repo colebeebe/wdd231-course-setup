0da1fd75d2e9b857e8d6d5aa9b4493a3e055408c
outtaspacecode <cole.beebe@protonmail.com>
2026-02-21 19:09:48 -0500

    Started work on w07

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,26 @@ import { mediaCardTemplate } from "./modules/templates.mjs";
 import { setHeaderFooter } from "./modules/setHeaderFooter.mjs";
 import '../css/home.css';
 
+
+function enableNavigation() {
+    // Use a querySelector to get the menu buttons
+
+    // When the main menu button is clicked:
+    menuButton.addEventListener("click", (e) => {
+        let target = e.target;
+        // Toggle the show class on the global-nav
+
+        // Check to see if target is the button or something inside the button
+
+        // Check to see if we just opened or closed the menu
+
+        // If we opened it then set the aria-expanded attribute on the button to true
+
+        // If we closed it then set the aria-expanded attribute on the button to false
+    })
+}
+
+
 function setParkInfo(data) {
     const intro = document.querySelector(".intro");
     intro.innerHTML = `
