64927ba0550d54d01479645830b333d418b755ac
outtaspacecode <cole.beebe@protonmail.com>
2026-03-06 20:20:09 -0700

    Finished part 6

--- src/js/modules/navigation.mjs
+++ src/js/modules/navigation.mjs
@@ -1,31 +1,29 @@
+function mainMenuHandler(ev) {
+  document.querySelector('.global-nav').classList.toggle('show');
+  if (document.querySelector('.global-nav').classList.contains('show')) {
+    ev.target.setAttribute('aria-expanded', true);
+  }
+  else {
+    ev.target.setAttribute('aria-expanded', false);
+  }
+}
+
+function subMenuHandler(ev) {
+  ev.currentTarget
+    .closest('li')
+    .querySelector('.global-nav__submenu')
+    .classList.toggle('show');
+  ev.currentTarget.querySelector('.icon').classList.toggle('rotate');
+}
+
 export function enableNavigation() {
   const menuButton = document.querySelector("#global-nav-toggle");
   const subMenuToggles = document.querySelectorAll(
     ".global-nav__split-button__toggle"
   );
-  // when the main menu button is clicked:
-  menuButton.addEventListener("click", (ev) => {
-    // toggle the show class on the global-nav
-    document.querySelector(".global-nav").classList.toggle("show");
-    // check to see if we just opened or closed the menu
-    if (document.querySelector(".global-nav").classList.contains("show")) {
-      // if we opened it then set the aria-expanded attribute to true
-      menuButton.setAttribute("aria-expanded", true);
-    } else {
-      // if we closed it then set the aria-expanded attribute to false
-      menuButton.setAttribute("aria-expanded", false);
-    }
-  });
-  subMenuToggles.forEach((toggle) => {
-    //for each submenu toggle
-    toggle.addEventListener("click", (ev) => {
-      // find the closest li ancestor, then find the submenu inside of that li and toggle the show class
-      ev.currentTarget
-        .closest("li")
-        .querySelector(".global-nav__submenu")
-        .classList.toggle("show");
-      // toggle the rotate class on the button icon that was clicked
-      ev.currentTarget.querySelector(".icon").classList.toggle("rotate");
-    });
-  });
+
+  menuButton.addEventListener('click', mainMenuHandler);
+  subMenuToggles.forEach(toggle => {
+    toggle.addEventListener('click', subMenuHandler);
+  })
 }
--- src/js/modules/parkService.mjs
+++ src/js/modules/parkService.mjs
@@ -181,7 +181,7 @@ const park = {
 
 const baseUrl = "https://developer.nps.gov/api/v1/";
 const apiKey = import.meta.env.VITE_NPS_API_KEY;
-const parkCode = 'yell';
+const parkCode = 'chat';
 
 export const parkInfoLinks = [
   {
